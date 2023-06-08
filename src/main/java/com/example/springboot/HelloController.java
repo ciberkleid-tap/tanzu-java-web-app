71a60ad9c4b6703c4a1b07b96ee2359c8e17fad9
Cora Iberkleid <ciberkleid@gmail.com>
2023-06-08 09:34:56 -0400

    Do not require specific string in test

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		String s = "Spring Boot + Tanzu";
+		String s = "Pittsburgh";
 		return "Greetings from " + s + "!";
 	}
 
--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -19,13 +21,24 @@ class HelloControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    // @Test
+    // void index() throws Exception {
+    //     assertEquals("Greetings from Spring Boot + Tanzu!", controller.index());
+
+    //     mockMvc
+    //         .perform(get("/"))
+    //         .andExpect(status().isOk());
+    //         .andExpect(content().string("Greetings from Spring Boot + Tanzu!"));
+    // }
+
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Spring Boot + Tanzu!", controller.index());
-
+        assertNotNull(controller.index());
+        assertTrue(controller.index().length() > 0, "Message must be greater than 0 length");
+        
         mockMvc
             .perform(get("/"))
-            .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot + Tanzu!"));
+            .andExpect(status().isOk());
+            //.andExpect(content().string("Greetings from Spring Boot + Tanzu!"));
     }
 }
