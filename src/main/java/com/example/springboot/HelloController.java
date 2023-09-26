54cba438b1ec0f8661fd59bb41a9da892b27a28b
Cora Iberkleid <ciberkleid@gmail.com>
2023-09-25 22:39:16 -0400

    new msg

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		String s = "SpringOne at VMware Explore";
+		String s = "Australia & New Zealand";
 		return "Greetings from " + s + "!";
 	}
 
